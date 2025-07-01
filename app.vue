9c16f603c1de77bc6ae3a55911b08253337d6085
Nuyptcy <aichunghsueh@gmail.com>
2025-07-01 14:57:28 +0800

    pref: 優化初始頁面加載

--- app.vue
+++ app.vue
@@ -1,31 +1,15 @@
 <template>
-    <NuxtLayout name="head" :class="store.en_class" v-if="has_language_txt"/>
-    <main :key="store.pageKey" @click="store.method_nav = false" :class="store.en_class" v-if="has_language_txt">
-        <NuxtPage v-if="has_language_txt" />
-    </main>
-    <NuxtLayout name="footer" :class="store.en_class" v-if="has_language_txt"/>
+  <NuxtLayout name="head" :class="store.en_class"/>
+  <main :key="store.pageKey" @click="store.method_nav = false" :class="store.en_class">
+      <NuxtPage />
+  </main>
+  <NuxtLayout name="footer" :class="store.en_class"/>
 
-    <NuxtLayout name="loading"/>
+  <NuxtLayout name="loading"/>
 
 </template>
 
 
 <script setup>
 const store = useStore()
-
-const has_language_txt = ref(false)
-
-
-
-
-onMounted(()=>{
-    if (store.language_txt.hasOwnProperty('auth') &&
-        store.language_txt.hasOwnProperty('member') &&
-        store.language_txt.hasOwnProperty('order')) {
-        has_language_txt.value = true
-    } 
-    console.log(has_language_txt.value);
-    
-})
-
 </script>

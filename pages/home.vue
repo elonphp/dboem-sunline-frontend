6fff72a4646f61e6300b76d6639f2b8aed50a1fe
Nuyptcy <aichunghsueh@gmail.com>
2025-07-04 16:36:39 +0800

    feature: home頁 改成i18n

--- pages/home.vue
+++ pages/home.vue
@@ -18,21 +18,21 @@
                             <span class="fw-bold">{{item.created_at.slice(0, 16)}}</span>
                             <span class="tag m-0" :class="store.status_colors(item.status_code)">{{item.status_name}}</span>
                         </div>
-                        <div class="pt-2"><span class="fw-bold">{{store.language_txt.order.text_orders}}{{store.language_txt.order.column_code}}：</span>{{item.code }}</div>
+                        <div class="pt-2"><span class="fw-bold">{{ $t('order.column_code') }}：</span>{{item.code }}</div>
                         <div>
-                            <span class="fw-bold">{{store.language_txt.order.column_customer_name}}：</span>{{item.customer_name}}
+                            <span class="fw-bold">{{ $t('order.column_customer_name') }}：</span>{{item.customer_name}}
                         </div>
                         <div>
-                            <span class="fw-bold">{{store.language_txt.member.column_address1}}：</span>{{item.shipping_address1}}{{ item.shipping_address2 }}
+                            <span class="fw-bold">{{ $t('member.column_address1') }}：</span>{{item.shipping_address1}}{{ item.shipping_address2 }}
                         </div>
                         <div>
-                            <span class="fw-bold">{{store.language_txt.order.column_total_quantity}}：</span>{{item.quantity}}
+                            <span class="fw-bold">{{ $t('order.column_total_quantity') }}：</span>{{item.quantity}}
                         </div>
                         <div>
-                            <span class="fw-bold">{{store.language_txt.order.column_sqm}}：</span>{{item.sqm}}
+                            <span class="fw-bold">{{ $t('order.column_sqm') }}：</span>{{item.sqm}}
                         </div>
                         <div v-if="store.is_dealer || store.userData.parent_id == 0">
-                            <span class="fw-bold">{{store.language_txt.order.text_order_comment_identity_salesperson}}：</span>{{item.salesperson_name}}
+                            <span class="fw-bold">{{ $t('order.text_order_comment_identity_salesperson') }}：</span>{{item.salesperson_name}}
                         </div>
                         <i class="fa-solid fa-pen"></i>
                     </nuxt-link>
@@ -51,15 +51,16 @@
 const store = useStore()
 const order = ref()
 const role = store.role
+const { t } = useI18n()
 
 // 連結區塊
 const link = ref([
-    { icon: 'fa-plus', title: store.language_txt.order?.text_add_order, router: 'create_order', roles: ['salesperson', 'dealer','sunline'] },
-    { icon: 'fa-history', title: store.language_txt.order?.tab_data, router: 'order-list', roles: ['salesperson'] },
-    { icon: 'fa-clipboard-list', title: store.language_txt.order?.text_manage_order, router: 'order-list', roles: ['dealer','sunline'] },
-    { icon: 'fa-file-circle-check', title: store.language_txt.order?.text_confirmed_orders, router: 'order-list?status=DealerApproved', roles: ['salesperson'] },
-    { icon: 'fa-user-pen', title: store.language_txt.default?.text_basic_information, router: 'user-info', roles: ['salesperson', 'dealer','sunline'] },
-    { icon: 'fa-gear', title: store.language_txt.order?.text_option_setting, router: '/order-setting', roles: ['dealer','sunline'] },
+    { icon: 'fa-plus', title: t('order.text_add_order'), router: 'create_order', roles: ['salesperson', 'dealer','sunline'] },
+    { icon: 'fa-history', title: t('order.tab_data'), router: 'order-list', roles: ['salesperson'] },
+    { icon: 'fa-clipboard-list', title: t('order.text_manage_order'), router: 'order-list', roles: ['dealer','sunline'] },
+    { icon: 'fa-file-circle-check', title: t('order.text_confirmed_orders'), router: 'order-list?status=DealerApproved', roles: ['salesperson'] },
+    { icon: 'fa-user-pen', title: t('default.text_basic_information'), router: 'user-info', roles: ['salesperson', 'dealer','sunline'] },
+    { icon: 'fa-gear', title: t('order.text_option_setting'), router: '/order-setting', roles: ['dealer','sunline'] },
 ])
 
 const is_access = (item)=>{

7fe41c746b8a9d0994a535c0eb285b6db24a61ff
Nuyptcy <aichunghsueh@gmail.com>
2025-07-24 18:13:43 +0800

    feature: 改成用以存的userData資料

--- pages/create_order/index.vue
+++ pages/create_order/index.vue
@@ -6,56 +6,56 @@
                 <div class="my-4">
                     <nuxt-link :to="prev_page" class="btn link">{{ $t('default.text_back') }}</nuxt-link>
                 </div>
-                <div class="row" v-if="userinfo">
+                <div class="row" v-if="userData">
                     <div class="col-lg-6 px-4 mb-lg-0 mb-4">
                         <h3 class="title fs-3 mb-4">{{$t('order.text_salesperson_information')}}</h3>
                         <div class="row bg-body p-4" style="border-radius: 5px;">
                             <div class="col-xxl-6 col-12  mb-2">
                                 <div class="user-data">
                                     <span class="col-4 ">{{$t('order.column_dealer_code')}}</span>
-                                    <div class="col" v-if="!order_id">{{ userinfo.employer_company_code }}</div>
+                                    <div class="col" v-if="!order_id">{{ userData.employer_company_code }}</div>
                                     <div class="col" v-else>{{ order_info.dealer_code }}</div>
                                 </div>
                             </div>
                             <div class="col-xxl-6 col-12  mb-2">
                                 <div class="user-data">
                                      <span class="col-4 ">{{$t('order.column_tax_id_num')}}</span>
-                                    <div class="col" v-if="!order_id">{{ userinfo.tax_id_num }}</div>
+                                    <div class="col" v-if="!order_id">{{ userData.tax_id_num }}</div>
                                     <div class="col" v-else>{{ order_info.dealer?.tax_id_number }}</div>
                                 </div>
                             </div>
                             <div class="col-md-12  mb-2">
                                 <div class="user-data">
                                     <span class="col-xxl-2 col-4">{{$t('order.column_company_name')}}</span>
-                                    <div class="col" v-if="!order_id">{{ userinfo.employer_company_name }}</div>
+                                    <div class="col" v-if="!order_id">{{ userData.employer_company_name }}</div>
                                     <div class="col" v-else>{{ order_info.dealer_name }}</div>
                                 </div>
                             </div>
                             <div class="col-xxl-6 col-12  mb-2">
                                 <div class="user-data">
                                     <span class="col-4">{{$t('order.column_salesperson_name')}}</span>
-                                    <div class="col" v-if="!order_id">{{ userinfo.name }}</div>
+                                    <div class="col" v-if="!order_id">{{ userData.name }}</div>
                                     <div class="col" v-else>{{ order_info.salesperson_name }}</div>
                                 </div>
                             </div>
                             <div class="col-xxl-6 col-12  mb-2">
                                 <div class="user-data">
                                      <span class="col-4 ">{{$t('order.column_salesperson_email')}}</span>
-                                    <div class="col" v-if="!order_id">{{ userinfo.email }}</div>
+                                    <div class="col" v-if="!order_id">{{ userData.email }}</div>
                                     <div class="col" v-else>{{ order_info.salesperson_email }}</div>
                                 </div>
                             </div>
                             <div class="col-xxl-6 col-12  mb-2">
                                 <div class="user-data">
                                      <span class="col-4 ">{{$t('order.column_salesperson_mobile')}}</span>
-                                    <div class="col" v-if="!order_id">{{ userinfo.mobile }}</div>
+                                    <div class="col" v-if="!order_id">{{ userData.mobile }}</div>
                                     <div class="col" v-else>{{ order_info.salesperson_mobile }}</div>
                                 </div>
                             </div>
                             <div class="col-xxl-6 col-12  mb-2">
                                 <div class="user-data">
                                      <span class="col-4 ">{{$t('order.column_salesperson_telephone')}}</span>
-                                    <div class="col" v-if="!order_id">{{ userinfo.telephone }}</div>
+                                    <div class="col" v-if="!order_id">{{ userData.telephone }}</div>
                                     <div class="col" v-else>{{ order_info.salesperson_telephone }}</div>
                                 </div>
                             </div>
@@ -63,7 +63,7 @@
                                 <div class="user-data">
                                      <span class="col-4 ">{{$t('member.column_country')}}</span>
                                     <div class="col" v-if="is_ship_to_me">
-                                        <div v-if="!order_id">{{ userinfo.country_name }}</div>
+                                        <div v-if="!order_id">{{ userData.country_name }}</div>
                                         <div v-else>{{ order_info.shipping_country_code }}</div>
                                     </div>
                                     <div class="col" v-else></div>
@@ -73,7 +73,7 @@
                                 <div class="user-data">
                                      <span class="col-4 ">{{$t('member.column_zip_code')}}</span>
                                     <div class="col" v-if="is_ship_to_me">
-                                        <div v-if="!order_id">{{ userinfo.zip_code }}</div>
+                                        <div v-if="!order_id">{{ userData.zip_code }}</div>
                                         <div v-else>{{ order_info.shipping_zip_code }}</div>
                                     </div>
                                     <div class="col" v-else></div>
@@ -83,7 +83,7 @@
                                 <div class="user-data">
                                      <span class="col-4 ">{{$t('member.column_state')}}</span>
                                     <div class="col" v-if="is_ship_to_me">
-                                        <div v-if="!order_id">{{ userinfo.state }}</div>
+                                        <div v-if="!order_id">{{ userData.state }}</div>
                                         <div v-else>{{ order_info.shipping_state }}</div>
                                     </div>
                                     <div class="col" v-else></div>
@@ -93,7 +93,7 @@
                                 <div class="user-data">
                                      <span class="col-4 ">{{$t('member.column_city')}}</span>
                                      <div class="col" v-if="is_ship_to_me">
-                                        <div v-if="!order_id">{{ userinfo.city }}</div>
+                                        <div v-if="!order_id">{{ userData.city }}</div>
                                         <div v-else>{{ order_info.shipping_city }}</div>
                                      </div>
                                      <div class="col" v-else></div>
@@ -103,7 +103,7 @@
                                 <div class="user-data">
                                     <span class="col-2">{{$t('member.column_address1')}}</span>
                                     <div class="col" v-if="is_ship_to_me">
-                                        <div v-if="!order_id">{{ userinfo.address1 }}</div>
+                                        <div v-if="!order_id">{{ userData.address1 }}</div>
                                         <div v-else>{{ order_info.shipping_address1 }}</div>
                                     </div>
                                     <div class="col" v-else></div>
@@ -113,7 +113,7 @@
                                 <div class="user-data">
                                     <span class="col-2">{{$t('member.column_address2')}}</span>
                                     <div class="col" v-if="is_ship_to_me">
-                                        <div v-if="!order_id">{{ userinfo.address2 }}</div>
+                                        <div v-if="!order_id">{{ userData.address2 }}</div>
                                         <div v-else>{{ order_info.shipping_address2 }}</div>
                                     </div>
                                     <div class="col" v-else></div>
@@ -202,11 +202,11 @@
 <script setup>
 const { locale } = useI18n()
 const store = useStore()
+const { userData } = storeToRefs(store)
 const router = useRouter()
 const route = useRoute()
 const order_id = route.query.id
 
-const userinfo = ref()
 
 const order_info = ref({})
 
@@ -237,36 +237,36 @@ const onSubmit = async(values)=>{
         // 運送方式
         delivery_method:values.delivery_method,
         // 經銷商(資訊)
-        dealer_id:userinfo.value.employer_company_id,
-        dealer_name:userinfo.value.employer_company_name,
-        employer_company_code:userinfo.value.employer_company_code,
+        dealer_id:userData.value.employer_company_id,
+        dealer_name:userData.value.employer_company_name,
+        employer_company_code:userData.value.employer_company_code,
         // 業務
-        salesperson_id:userinfo.value.id,
-        salesperson_first_name:userinfo.value.first_name,
-        salesperson_last_name:userinfo.value.last_name,
-        salesperson_email:userinfo.value.email,
-        salesperson_telephone:userinfo.value.telephone || "",
-        salesperson_mobile:userinfo.value.mobile || "",
+        salesperson_id:userData.value.id,
+        salesperson_first_name:userData.value.first_name,
+        salesperson_last_name:userData.value.last_name,
+        salesperson_email:userData.value.email,
+        salesperson_telephone:userData.value.telephone || "",
+        salesperson_mobile:userData.value.mobile || "",
         
         // 客戶
         customer_first_name:values.customer_first_name,
         customer_last_name:values.customer_last_name,
         shipping_recipient:values.customer_last_name + values.customer_first_name,
-        customer_email:userinfo.value.email,
+        customer_email:userData.value.email,
         status_code:"Draft",
         side_mark:values.side_mark,
         note:values.note || "",
-        tax:userinfo.value.tax_id_number || "",
+        tax:userData.value.tax_id_number || "",
     }
-    if(is_ship_to_me.value && (order_info.value.salesperson_id == userinfo.value.user_id || !order_id)){
+    if(is_ship_to_me.value && (order_info.value.salesperson_id == userData.value.user_id || !order_id)){
         // 運送
-        data.shipping_country_code = userinfo.value.country_code || ""
-        // data.shipping_country_name = userinfo.value.country_name || "",
-        data.shipping_zip_code = userinfo.value.zip_code || ""
-        data.shipping_state = userinfo.value.state || ""
-        data.shipping_city = userinfo.value.city || ""
-        data.shipping_address1 = userinfo.value.address1 || ""
-        data.shipping_address2 = userinfo.value.address2 || ""
+        data.shipping_country_code = userData.value.country_code || ""
+        // data.shipping_country_name = userData.value.country_name || "",
+        data.shipping_zip_code = userData.value.zip_code || ""
+        data.shipping_state = userData.value.state || ""
+        data.shipping_city = userData.value.city || ""
+        data.shipping_address1 = userData.value.address1 || ""
+        data.shipping_address2 = userData.value.address2 || ""
     }
     if(order_id){
         data.order_id = order_id
@@ -281,8 +281,8 @@ const onSubmit = async(values)=>{
             method:"POST",
             headers:{
                 "Content-Type": "application/json",
-                "Authorization": "Bearer " + store.userData.access_token,
-                "X-CLIENT-IPV4":store.userData.loginIpAddress
+                "Authorization": "Bearer " + userData.value.access_token,
+                "X-CLIENT-IPV4":userData.value.loginIpAddress
             },
             body:JSON.stringify(data)
         })
@@ -303,11 +303,6 @@ const onSubmit = async(values)=>{
 }
 
 // 會員資料
-const get_data = async()=>{
-    const data = await store.get_user_data()
-    userinfo.value = data
-    // console.log('會員',userinfo.value);
-}
 const { $j } = useNuxtApp()
 
 // 訂單資料
@@ -316,7 +311,7 @@ const get_order_data = async()=>{
     try{
         const res = await fetch(url,{
             headers:{
-                "Authorization": "Bearer " + store.userData.access_token
+                "Authorization": "Bearer " + userData.value.access_token
             },
         })
         const res_data = await res.json()
@@ -337,14 +332,9 @@ const get_order_data = async()=>{
     }
 }
 
-
-
-store.show_loading(true)
 onMounted(async()=>{
-    await get_data()
     if(order_id){
         await get_order_data()
     }
-    store.show_loading(false)
 })
 </script>
